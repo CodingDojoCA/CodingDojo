1d37f069d5885d16dd9181019c4e0326b802fca2
mateusz-g <mateusz.glapinski@gmail.com>
2014-05-29 14:47:27 +0200

    change of repo names

--- Tennis/TddTennis/src/com/comarch/tdd/tenis/CannotAddPointException.java
+++
@@ -1,7 +0,0 @@
-package com.comarch.tdd.tenis;
-
-/**
- * Created by Kleko on 2014-05-23.
- */
-public class CannotAddPointException extends Exception {
-}
--- Tennis/TennisKata/src/com/comarch/exception/GemFinishedException.java
+++
@@ -1,9 +0,0 @@
-package com.comarch.exception;
-
-public class GemFinishedException extends Exception {
-
-	public GemFinishedException(String message) {
-		super(message);
-	}
-
-}
